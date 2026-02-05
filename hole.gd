4214cc24f1aff7546a43e69ab5317ddafd26b5b2
Alec Singer <sosenteam@gmail.com>
2026-02-05 08:16:09 -0800

    added 3rd level + changed ball physics + required low speed for hole

--- hole.gd
+++ hole.gd
@@ -2,5 +2,6 @@ extends Sprite2D
 
 
 func _on_hole_area_body_entered(body: Node2D) -> void:
-	EventBus.next_hole()
+	if(get_node("../../PlayerBall").linear_velocity.length() < 300):
+		EventBus.next_hole()
 	
